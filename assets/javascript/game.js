f6dd60e93164b00798ce2fc406734d005069d1df
Joan Marie Verba <verba001@umn.edu>
2018-03-01 13:48:51 -0600

    progress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,20 +9,35 @@ let currentWord = wordList[gameNumber];
 let currentWordArray = currentWord.split('');
 let numberOfLetters = currentWordArray.length;
 let displayWord = ["_"];
+let alreadyGuessed = " ";
 let i = 1;
 
 for (i = 1; i < numberOfLetters; i++) {
     displayWord.push("_");
 }
 
+// display current word with underscores in place of letters
 let display = displayWord.toString();
 document.getElementById("demo").innerHTML = display.replace(/,/g , " ");
 
+//display number of guesses remaining
+document.getElementById("numguess").innerHTML = guessNumber;
+
+//display letters already guessed
+document.getElementById("already").innerHTML = alreadyGuessed;
+
+
+let currentGuess = prompt("Input one letter ").toLowerCase();
+
+guessNumber = guessNumber - 1;
 
 for (i = 0; i < numberOfLetters; i++) {
-
+    if (currentWordArray[i].toLowerCase() === currentGuess) {
+        displayWord[i] = currentGuess;
+    }
 }
 
+console.log(displayWord);
 
 // document.addEventListener('keypress', (event) => {
 //   const keyName = event.key;
