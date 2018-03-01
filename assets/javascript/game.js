43eadfa1021d55faa0a9ac6dcd62a9327cff93f2
Joan Marie Verba <verba001@umn.edu>
2018-03-01 13:15:49 -0600

    first steps

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,25 +1,32 @@
 "use strict"
 
-
-const useful = "string";
-
-function anotherFunction() {
-    return "usefulstuff";
-}
-const usefullStuff = anotherFunction();
-
-const palindromeChecker = function (sentence) {
-    const input = sentence.toLowerCase().replace(/[\s.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
-    const result = sentence.split('').reverse().join('').toLowerCase().replace(/[\s.,\/#!$%\^&\*;:{}=\-_`~()]/g, "");
-    console.log(input);
-    console.log(result);
-    console.log (result === input);
-    console.log (usefullStuff); //this is how to access stuff from another function in this function
-}
-
-palindromeChecker ("wow");
-palindromeChecker ("Was it a car or a cat I saw")
-palindromeChecker("A man, a plan, a canal, Panama")
-palindromeChecker ("This is a palindrome!")
+let gameNumber = 0;
+let guessNumber = 12;
 
 const wordList = ["Mercury", "Venus", "Earth", "Moon", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto", "Sun", "Stars", "Galaxy", "Supernova", "Asteroids", "Titan", "Nebula", "Orbit", "Comet", "Satellite"]
+
+let currentWord = wordList[gameNumber];
+let currentWordArray = currentWord.split('');
+let numberOfLetters = currentWordArray.length;
+let displayWord = ["_"];
+let i = 1;
+
+for (i = 1; i < numberOfLetters; i++) {
+    displayWord.push("_");
+}
+
+let display = displayWord.toString();
+document.getElementById("demo").innerHTML = display.replace(/,/g , " ");
+
+
+for (i = 0; i < numberOfLetters; i++) {
+
+}
+
+
+// document.addEventListener('keypress', (event) => {
+//   const keyName = event.key;
+
+//   alert('keypress event\n\n' + 'key: ' + keyName);
+// });
+
