35b5fc68918aea91b9db6ad70c1132947ab8ceac
Joan Marie Verba <verba001@umn.edu>
2018-03-03 16:20:14 -0600

    more updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,11 @@
 "use strict"
 
 let gameNumber = 0;
-let guessNumber = 12;
+let numberOfGuesses = 12;
 
 const wordList = ["Mercury", "Venus", "Earth", "Moon", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto", "Sun", "Stars", "Galaxy", "Supernova", "Asteroids", "Titan", "Nebula", "Orbit", "Comet", "Satellite"]
-const success = " ";
+let done = false;
+let message = " ";
 
 let currentWord = wordList[gameNumber];
 let currentWordArray = currentWord.split('');
@@ -17,40 +18,60 @@ for (i = 1; i < numberOfLetters; i++) {
     displayWord.push("_");
 }
 
-while (success === " " && guessNumber > 0) {
+document.onkeyup = function (letter) {
+    // inside here lives some
+    // code that will run every time
+    // a keyup event happens.
+    let keyPressed = letter.key;
+    console.log(keyPressed);
+    return keyPressed;
+}
+
+
+function Hangman () {
+    console.log("done ", done, "number of guesses ", numberOfGuesses)
+    if (!done && numberOfGuesses > 0) {
         // display current word with underscores in place of letters
         let display = displayWord.toString();
         $("#demo").text(display.replace(/,/g, " "));
 
+        console.log (display);
+
         //display number of guesses remaining
-        $("#numguess").text(guessNumber);
+        $("#numguess").text(numberOfGuesses);
 
         //display letters already guessed
         $("#already").text(alreadyGuessed);
 
         //enter a guess
-        let currentGuess = prompt("Input one letter ").toLowerCase();
+        //  let currentGuess = prompt("Input one letter ").toLowerCase();
 
-        guessNumber = guessNumber - 1;
+        let currentGuess = document.querySelector("input").value;
+
+        numberOfGuesses = numberOfGuesses - 1;
 
         for (i = 0; i < numberOfLetters; i++) {
             if (currentWordArray[i].toLowerCase() === currentGuess) {
                 displayWord[i] = currentGuess;
             }
         }
-        alreadyGuessed = alreadyGuessed.concat(currentGuess) + " ";
+
+        console.log ("alreadyGuessed ", alreadyGuessed, "currentGuess ", currentGuess);
+        alreadyGuessed = alreadyGuessed.concat(currentGuess);
 
         if (currentWord.toString() === displayWord.toString()) {
-            success = "SUCCESS!!"
-            $("#gameover").text(success);
+             message = "SUCCESS!!"
+            $("#gameover").text(message);
+            done = true;
             
         }
-        if (guessNumber === 0) {
-            success = `Sorry, you've run out of guesses. The word was ${wordList[gameNumber]}`;
-            $("#gameover").text(success);
+        if (numberOfGuesses === 0) {
+            message = `Sorry, you've run out of guesses. The word was ${wordList[gameNumber]}`;
+            $("#gameover").text(message);
+            done = true;
         }
+    }
+
 }
-
-
 
 
