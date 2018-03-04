886345ae6b5f7dc574034a86d1e326717f3c576a
Joan Marie Verba <verba001@umn.edu>
2018-03-03 20:03:09 -0600

    success

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,6 +5,7 @@ let numberOfGuesses = 12;
 
 const wordList = ["Mercury", "Venus", "Earth", "Moon", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto", "Sun", "Stars", "Galaxy", "Supernova", "Asteroids", "Titan", "Nebula", "Orbit", "Comet", "Satellite"]
 let done = false;
+let notFirst = false;
 let message = " ";
 
 let currentWord = wordList[gameNumber];
@@ -18,52 +19,49 @@ for (i = 1; i < numberOfLetters; i++) {
     displayWord.push("_");
 }
 
-document.onkeyup = function (letter) {
-    // inside here lives some
-    // code that will run every time
-    // a keyup event happens.
-    let keyPressed = letter.key;
-    console.log(keyPressed);
-    return keyPressed;
-}
-
-
-function Hangman () {
+function Hangman (currentGuess) {
     console.log("done ", done, "number of guesses ", numberOfGuesses)
     if (!done && numberOfGuesses > 0) {
         // display current word with underscores in place of letters
         let display = displayWord.toString();
-        $("#demo").text(display.replace(/,/g, " "));
+        if (!notFirst) {$("#demo").text(display.replace(/,/g, " "));}
 
         console.log (display);
 
         //display number of guesses remaining
         $("#numguess").text(numberOfGuesses);
 
-        //display letters already guessed
-        $("#already").text(alreadyGuessed);
-
-        //enter a guess
-        //  let currentGuess = prompt("Input one letter ").toLowerCase();
-
-        let currentGuess = document.querySelector("input").value;
+    
 
         numberOfGuesses = numberOfGuesses - 1;
 
         for (i = 0; i < numberOfLetters; i++) {
-            if (currentWordArray[i].toLowerCase() === currentGuess) {
+            if (notFirst && currentWordArray[i].toLowerCase() === currentGuess) {
                 displayWord[i] = currentGuess;
+                display = displayWord.toString();
             }
         }
 
+        //display letters already guessed
+        //revise display word
         console.log ("alreadyGuessed ", alreadyGuessed, "currentGuess ", currentGuess);
-        alreadyGuessed = alreadyGuessed.concat(currentGuess);
+        if (notFirst) {
+            alreadyGuessed = alreadyGuessed.concat(currentGuess);
+            $("#demo").text(display.replace(/,/g, " ")); 
+            $("#already").text(alreadyGuessed);
+        };
+      
+        
 
-        if (currentWord.toString() === displayWord.toString()) {
+       
+       
+        
+        console.log("currentWord ", currentWord, "displayWord ", displayWord.toString().replace(/,/g, ""));
+        if (currentWord.toString().toLowerCase() === displayWord.toString().replace(/,/g, "")) {
              message = "SUCCESS!!"
             $("#gameover").text(message);
             done = true;
-            
+
         }
         if (numberOfGuesses === 0) {
             message = `Sorry, you've run out of guesses. The word was ${wordList[gameNumber]}`;
@@ -71,6 +69,17 @@ function Hangman () {
             done = true;
         }
     }
+    notFirst = true;
+}
+
+
+// Captures keyboard input. Depending on the letter pressed it will "call" (execute) different functions.
+document.onkeyup = function (event) {
+
+    // Captures the key press, converts it to lowercase, and saves it to a variable.
+    var letter = String.fromCharCode(event.which).toLowerCase();
+
+    Hangman(letter);
 
 }
 
